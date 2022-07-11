c7a435d7250f5a2f330b74e9588331956567053a
Tran Long <long.pham.tran2612@gmail.com>
2022-07-11 10:55:15 +0700

    ++ change graphql tool -> dsg netflix

--- src/main/java/net/lvs/demo_graphql/graphql/AuthorDataFetcher.java
+++ src/main/java/net/lvs/demo_graphql/graphql/AuthorDataFetcher.java
@@ -1,31 +1,47 @@
-package net.lvs.demo_graphql.service.mutation;
+package net.lvs.demo_graphql.graphql;
 
-import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
 import net.lvs.demo_graphql.model.Author;
 import net.lvs.demo_graphql.repository.AuthorRepository;
-import net.lvs.demo_graphql.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
-@Component
-public class AuthorMutation implements GraphQLMutationResolver {
-
-
+/**
+ * 10:36 AM 11-Jul-22
+ * Long Tran
+ */
+@DgsComponent
+public class AuthorDataFetcher {
 
     @Autowired
-    private  AuthorRepository authorRepository;
+    private AuthorRepository authorRepository;
 
+    @DgsQuery
+    public Author findAuthorById(String id) {
+        return authorRepository.findById(id).get();
+    }
+
+    @DgsQuery
+    public List<Author> findAllAuthor() {
+        return authorRepository.findAll();
+    }
+
+    @DgsMutation
     public Author createAuthor(Author author) {
         return  authorRepository.save(author);
     }
 
+    @DgsMutation
     public boolean deleteAuthor(String id) {
         authorRepository.deleteById(id);
         return true;
     }
 
+    @DgsMutation
     public Author updateAuthor( String id,String firstName, String lastName) {
         Optional<Author> opt = authorRepository.findById(id);
         if (opt.isPresent()) {
@@ -36,6 +52,5 @@ public class AuthorMutation implements GraphQLMutationResolver {
             return author;
         }
         return null;
-
     }
 }
--- src/main/java/net/lvs/demo_graphql/graphql/BookDataFetcher.java
+++ src/main/java/net/lvs/demo_graphql/graphql/BookDataFetcher.java
@@ -1,23 +1,41 @@
-package net.lvs.demo_graphql.service.mutation;
+package net.lvs.demo_graphql.graphql;
 
-import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
 import net.lvs.demo_graphql.model.Author;
 import net.lvs.demo_graphql.model.Book;
 import net.lvs.demo_graphql.model.dto.BookDTO;
 import net.lvs.demo_graphql.repository.AuthorRepository;
 import net.lvs.demo_graphql.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
-@Component
-public class BookMutation implements GraphQLMutationResolver {
+/**
+ * 10:35 AM 11-Jul-22
+ * Long Tran
+ */
+@DgsComponent
+public class BookDataFetcher {
 
     @Autowired
     private BookRepository bookRepository;
     @Autowired
     private AuthorRepository authorRepository;
+
+    @DgsQuery
+    public Book findBookById(String id) {
+        return bookRepository.findById(id).get();
+    }
+
+    @DgsQuery
+    public List<Book> findAllBook() {
+        return bookRepository.findAll();
+    }
+
+    @DgsMutation
     public Book newBook(BookDTO bookDTO) {
         Book book = new Book();
         book.setName(bookDTO.getName());
@@ -26,10 +44,12 @@ public class BookMutation implements GraphQLMutationResolver {
         author.ifPresent(book::setAuthor);
         return  bookRepository.save(book);
     }
+    @DgsMutation
     public boolean deleteBook(String id) {
         bookRepository.deleteById(id);
         return true;
     }
+    @DgsMutation
     public Book updateBook(String id,String name,int page) {
         Optional<Book> opt = bookRepository.findById(id);
         if (opt.isPresent()) {
@@ -41,4 +61,6 @@ public class BookMutation implements GraphQLMutationResolver {
         }
         return null;
     }
+
+
 }
